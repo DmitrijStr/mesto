5a63479c71465476a0b998052eb44735d641572e
Dmitrij <s.d144@yandex.by>
2020-08-03 14:11:03 +0300

    project 6 v1.3 review fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,26 @@
-const initialCards = [
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
@@ -29,6 +28,7 @@ const initialCards = [
 const editModal = document.querySelector('.popup_type_edit');
 const newCardModal = document.querySelector('.popup_type_new-card');
 const imageModal = document.querySelector('.popup_type_image');
+const overlay = document.querySelectorAll('.pop-up');
 
 //кнопки открытия модальных окон
 const editButton = document.querySelector('.profile__button_action_edit');
@@ -52,77 +52,105 @@ const urlInput = addCard.querySelector('.pop-up__text_type_link');
 const imageModalImage = imageModal.querySelector('.pop-up__image');
 const imageModalPlace = imageModal.querySelector('.pop-up__image-name');
 
-// функция открытия модального окна
-function toggleModal(modal) {
-  if (!modal.classList.contains('pop-up_type_opened')) {
-    nameInput.value = name.textContent;
-    jobInput.value = profession.textContent;
-  }
-  modal.classList.toggle('pop-up_type_opened');
-};
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  name.textContent = nameInput.value;
-  profession.textContent = jobInput.value;
-  toggleModal(editModal);
-};
-
-function addCardHandler (evt) {
-  evt.preventDefault();
-  renderCard({name: placeInput.value, link: urlInput.value});
-  toggleModal(newCardModal);
-  placeInput.value = '';
-  urlInput.value = '';
-};
-
-formElement.addEventListener('submit', formSubmitHandler);
-addCard.addEventListener('submit', addCardHandler);
-
-
-
-
-const profileName = document.querySelector('.profile__name');
-const profileProfession = document.querySelector('.profile__profession');
-const saveButton = document.querySelector('.pop-up__btn_action_save');
+const newCardsaveButton = newCardModal.querySelector('.pop-up__btn_action_save');
 const name = document.querySelector('.profile__name');
 const profession = document.querySelector('.profile__profession');
 
-// слушатели открытия модальных окон
-editButton.addEventListener('click', () => {
-  toggleModal(editModal)
+// template 
+const elementTemplate = document.querySelector('.card-template').content.querySelector('.photo-grid__item');
+const grid = document.querySelector('.photo-grid');
 
-});
-plusButton.addEventListener('click', () => {
-  toggleModal(newCardModal)
-});
 
-// слушатели закрытия модальных окон
-editModalCloseBtn.addEventListener('click', () => {
-  toggleModal(editModal)
-});
-newCardModalCloseBtn.addEventListener('click', () =>{
-  toggleModal(newCardModal)
-});
-imageModalCloseBtn.addEventListener('click', () => {
-  toggleModal(imageModal)
-});
+// функции открытия и закрытия модального окна
+
+function openModal(modal) {
+  if (!modal.classList.contains('pop-up_type_opened')) {
+    modal.classList.add('pop-up_type_opened');
+    document.body.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        closeModal(modal);
+      }
+    });
+  }
+}
+// Спасибо больше за ваши комментарии, они очень помогли, постарался исправить все неточности (:
+
+function closeModal(modal) {
+  if (modal.classList.contains('pop-up_type_opened')) {
+    modal.classList.remove('pop-up_type_opened');
+  }
+}
+
+function profieEdit() {
+  nameInput.value = name.textContent;
+  jobInput.value = profession.textContent;
+  openModal(editModal);
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  name.textContent = nameInput.value;
+  profession.textContent = jobInput.value;
+  closeModal(editModal);
+}
+
+function addCardHandler(evt) {
+  evt.preventDefault();
+  renderCard({
+    name: placeInput.value,
+    link: urlInput.value
+  });
+  closeModal(newCardModal);
+  placeInput.value = '';
+  urlInput.value = '';
+  newCardsaveButton.setAttribute('disabled', true);
+  newCardsaveButton.classList.add(validationClasses.inactiveButtonClass);
+}
 
 //функция лайка карточки
 function likeClick(button) {
   button.classList.toggle('photo-grid__like-button_type_active');
 };
 
-//функция открытия изображения
-function imageClick() {
-  toggleModal(imageModal);
-};
+// закрытие по оверлей
+function closeOverlay(evt) {
+  if (evt.target.classList.contains('pop-up_type_opened')) {
+    closeModal(evt.target);
+  }
+}
 
-// template 
-const elementTemplate = document.querySelector('.card-template').content.querySelector('.photo-grid__item');
-const grid = document.querySelector('.photo-grid');
+// for (let i = 0; i < overlay.length; i++) {
+//   overlay[i].addEventListener('click', closeOverlay);
+// }
 
-function createCard (data) {
+overlay.forEach((everyOverlay) => {
+  everyOverlay.addEventListener('click', closeOverlay);
+});
+
+formElement.addEventListener('submit', formSubmitHandler);
+addCard.addEventListener('submit', addCardHandler);
+
+// слушатели открытия модальных окон
+editButton.addEventListener('click', profieEdit);
+
+plusButton.addEventListener('click', () => {
+  openModal(newCardModal)
+  placeInput.value = '';
+  urlInput.value = '';
+});
+
+// слушатели закрытия модальных окон
+editModalCloseBtn.addEventListener('click', () => {
+  closeModal(editModal);
+});
+newCardModalCloseBtn.addEventListener('click', () => {
+  closeModal(newCardModal);
+});
+imageModalCloseBtn.addEventListener('click', () => {
+  closeModal(imageModal)
+});
+
+function createCard(data) {
   const cardElement = elementTemplate.cloneNode(true);
   const image = cardElement.querySelector('.photo-grid__image');
   const place = cardElement.querySelector('.photo-grid__place');
@@ -131,19 +159,21 @@ function createCard (data) {
   const imageButton = cardElement.querySelector('.photo-grid__image-button');
   image.src = data.link;
   place.textContent = data.name;
+  image.alt = `изображение ${data.name}`;
   likeButton.addEventListener('click', () => {
     likeClick(likeButton);
   });
   deleteButton.addEventListener('click', () => {
-    cardElement.remove(); 
+    cardElement.remove();
   })
   imageButton.addEventListener('click', () => {
     imageModalImage.src = image.src;
     imageModalPlace.textContent = place.textContent;
-    imageClick();
+    imageModalImage.alt = `изображение ${place.textContent}`;
+    openModal(imageModal);
   });
   return cardElement;
-};
+}
 
 function renderCard(data) {
   grid.prepend(createCard(data));
@@ -151,33 +181,18 @@ function renderCard(data) {
 
 initialCards.forEach((data) => {
   renderCard(data);
-})
-
-// //6 проектная 
-
-// закрытие по оверлей
-
-const overlay = document.querySelectorAll('.pop-up');
-
-function closeOverlay(evt) {
-  if (evt.target.classList.contains('pop-up_type_opened')) {
-    evt.target.classList.remove('pop-up_type_opened')
-  }
-}
-
-document.body.addEventListener('keydown', function(evt) {
-  if (evt.key === 'Escape') {
-    if (overlay[0].classList.contains('pop-up_type_opened')) {
-      overlay[0].classList.remove('pop-up_type_opened')
-    } else if (overlay[1].classList.contains('pop-up_type_opened')) {
-      overlay[1].classList.remove('pop-up_type_opened')
-    } else if (overlay[2].classList.contains('pop-up_type_opened')) {
-      overlay[2].classList.remove('pop-up_type_opened')
-    }
-  }
-})
+});
 
 
-for (let i = 0; i < overlay.length; i++) {
-  overlay[i].addEventListener('click', closeOverlay);
-}
+
+// document.body.addEventListener('keydown', function (evt) {
+//   if (evt.key === 'Escape') {
+//     if (overlay[0].classList.contains('pop-up_type_opened')) {
+//       overlay[0].classList.remove('pop-up_type_opened')
+//     } else if (overlay[1].classList.contains('pop-up_type_opened')) {
+//       overlay[1].classList.remove('pop-up_type_opened')
+//     } else if (overlay[2].classList.contains('pop-up_type_opened')) {
+//       overlay[2].classList.remove('pop-up_type_opened')
+//     }
+//   }
+// })
